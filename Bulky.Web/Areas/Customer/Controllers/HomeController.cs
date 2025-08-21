eab7e07c5a20e312b85b9dab4346291d55b320f2
Ibrahim-Mohamed-66 <ibrahim.mohamed6005@gmail.com>
2025-08-21 03:30:21 +0300

    Add initial layout for home page

--- Bulky.Web/Areas/Customer/Controllers/HomeController.cs
+++ Bulky.Web/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Bulky.DataAccess.Repositories.IRepositories;
 using Bulky.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,16 +9,50 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 10)
         {
-            return View();
+            var totalProducts = await _unitOfWork.Product.GetCountAsync(p => p.IsHidden == false);
+
+            var totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+            page = Math.Max(1, Math.Min(page, totalPages));
+
+            var products = await _unitOfWork.Product.GetAllAsync(
+                filter: p => p.IsHidden == false,
+                includes: p => p.Category,
+                orderBy: q => q.OrderBy(p => p.DisplayOrder),
+                pageNumber: page,
+                pageSize: pageSize
+            );
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.PageSize = pageSize;
+            ViewBag.TotalProducts = totalProducts;
+            ViewBag.HasPreviousPage = page > 1;
+            ViewBag.HasNextPage = page < totalPages;
+
+            return View(products);
         }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var products = await _unitOfWork.Product.GetByIdAsync(id, includes: p => p.Category);
+            if (products == null || products.IsHidden)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
 
         public IActionResult Privacy()
         {
